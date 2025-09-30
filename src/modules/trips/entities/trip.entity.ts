3b22cb7922c20035ca63cccccbfb6f545f07c38a
Rami <droll552@gmail.com>
2025-09-30 17:51:28 +0300

    trip params added

--- src/modules/trips/entities/trip.entity.ts
+++ src/modules/trips/entities/trip.entity.ts
@@ -1,3 +1,12 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
 export enum TripStatus {
     Completed = 'Completed',
     Planned = 'Planned',
@@ -6,17 +15,49 @@ export enum TripStatus {
     Canceled = 'Canceled',
 }
 
+@Entity('trips')
 export class Trip {
-    constructor(
-        public readonly id: string, //think about UUID
-        public userId: string, // from auth service
-        public name: string,
-        public description: string,
-        public startDate: Date,
-        public endDate: Date,
-        public status: TripStatus,
-        public isPublic: boolean,
-        public tags: string[],
-        public waypoints: Waypoint[] = [],
-    ) {}
+    @PrimaryGeneratedColumn('uuid')
+    public readonly id: string; //ToDo: think about using UUID type. Check also if PrimaryGeneratedColumn validates input
+
+    @Column()
+    public userId: string; // from auth service
+
+    @Column()
+    public name: string;
+
+    @Column({ nullable: true })
+    public description: string;
+
+    @Column({ nullable: true, type: 'date' })
+    public startDate: Date;
+
+    @Column({ nullable: true, type: 'date' })
+    public endDate: Date;
+
+    @Column({
+        type: 'enum',
+        enum: TripStatus,
+        default: TripStatus.Draft,
+    })
+    public status: TripStatus;
+
+    @Column({ type: 'bool', default: false })
+    public isPublic: boolean;
+
+    @Column('simple-array', { nullable: true })
+    public tags: string[];
+
+    // (waypoint) => waypoint.trip Specifies which property in Waypoint points back to Trip, Tells TypeORM: "Each waypoint has a 'trip' property that references this Trip"
+    @OneToMany(() => Waypoint, (waypoint) => waypoint.trip, {
+        cascade: true, // Automatically save waypoints when saving trip. Means: "When I save/update/remove a Trip, also save/update/remove its waypoints"
+        eager: true, // Automatically load waypoints when loading trip
+    })
+    public waypoints: Waypoint[] = [];
+
+    @CreateDateColumn()
+    public createdAt: Date;
+
+    @UpdateDateColumn()
+    public updatedAt: Date;
 }
